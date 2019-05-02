dd02f180209fe2587b15ee55e32a204203aebbdb
Kikero1874 <rteuwens9206@gmail.com>
2019-05-02 18:55:23 +0200

    changed order of registering blueprints

--- website/__init__.py
+++ website/__init__.py
@@ -9,15 +9,15 @@ from config import app_config # used in the application factory
 def create_app(config_name):
 
     # initializing the app
-    app = Flask(__name__, instance_relative_config=True)
+    app = Flask(__name__)
     app.config.from_object(app_config[config_name])
 
     # importing and registering blueprints
-    from .auth import auth as auth_blueprint
-    app.register_blueprint(auth_blueprint, url_prefix='/auth')
-    
     from .landing_page import landing as landing_blueprint
     app.register_blueprint(landing_blueprint)
+
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint, url_prefix='/auth')
 
     # delivering the initialized application
     return app
